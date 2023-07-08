d35d19a7bb17cd91eeadbe0c667fc0bd9e7976cb
GoldenKingSama <129943074+GoldenKingSama@users.noreply.github.com>
2023-07-09 00:19:33 +0330

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,12 @@
+The program performs pixel sorting on an input image. 
+The user is prompted to provide the input image path 
+and the output image path. The program then applies 
+the brightness sorting algorithm to sort the pixels in 
+the image. Finally, it saves the sorted image to the 
+specified output image path.
+
+Before opening the program, you need to install these 
+two librarie:
+
+	pip install Pillow
+	pip install numpy
--- pixel_sorting.py
+++ pixel_sorting.py
@@ -1,29 +1,20 @@
-import numpy as np
 from PIL import Image
+import numpy as np
 
-def load_image(file_path):
-    image = Image.open(file_path)
-    return image
+input_path = input("Enter the input image path: ")
+output_path = input("Enter the output image path: ")
 
-def pixel_sort(image):
-    img_array = np.array(image)
-    height, width, _ = img_array.shape
+# Load Image
+image = Image.open(input_path)
+img_array = np.array(image)
+height, width, _ = img_array.shape
 
-    for row in range(height):
-        sorted_row = sorted(img_array[row], key=lambda pixel: sum(pixel))
-        img_array[row] = sorted_row
+# Pixel Sorting
+for row in range(height):
+    sorted_row = sorted(img_array[row], key=lambda pixel: sum(pixel))
+    img_array[row] = sorted_row
 
-    sorted_image = Image.fromarray(img_array)
-    return sorted_image
+sorted_image = Image.fromarray(img_array)
 
-def save_image(image, output_path):
-    image.save(output_path)
-
-if __name__ == "__main__":
-    input_path = input("Enter the input image path: ")
-    output_path = input("Enter the output image path: ")
-
-    original_image = load_image(input_path)
-    sorted_image = pixel_sort(original_image)
-
-    save_image(sorted_image, output_path)
+# Save Image
+sorted_image.save(output_path)
